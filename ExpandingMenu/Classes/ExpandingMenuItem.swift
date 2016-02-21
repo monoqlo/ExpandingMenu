7afe699a31025387fec696aac5fce29d5c04bd60
monoqlo <monoqlo44@gmail.com>
2016-02-21 11:53:32 +0900

    Enable ExpandingMenuItem customization by property. title, title color, whether the tapped action fires when title are tapped.

--- ExpandingMenu/Classes/ExpandingMenuItem.swift
+++ ExpandingMenu/Classes/ExpandingMenuItem.swift
@@ -7,7 +7,43 @@
 
 import UIKit
 
-public class ExpandingMenuItem: UIButton {
+public class ExpandingMenuItem: UIView {
+    
+    public var title: String? {
+        get {
+            return self.titleButton?.titleLabel?.text
+        }
+        
+        set {
+            if let title = newValue {
+                if let titleButton = self.titleButton {
+                    titleButton.setTitle(title, forState: .Normal)
+                } else {
+                    self.titleButton = self.createTitleButton(title, titleColor: self.titleColor)
+                }
+                
+                self.titleButton?.sizeToFit()
+            } else {
+                self.titleButton = nil
+            }
+        }
+    }
+    
+    public var titleColor: UIColor? {
+        get {
+            return self.titleButton?.titleColorForState(.Normal)
+        }
+        
+        set {
+            self.titleButton?.setTitleColor(newValue, forState: .Normal)
+        }
+    }
+    
+    public var titleTappedActionEnabled: Bool = true {
+        didSet {
+            self.titleButton?.userInteractionEnabled = titleTappedActionEnabled
+        }
+    }
     
     var index: Int = 0
     weak var delegate: ExpandingMenuButton?
@@ -16,7 +52,7 @@ public class ExpandingMenuItem: UIButton {
     private var tappedAction: (() -> Void)?
     
     // MARK: - Initializer
-    public init(size: CGSize?, title: String?, image: UIImage, highlightedImage: UIImage, backgroundImage: UIImage?, backgroundHighlightedImage: UIImage?, itemTapped: (() -> Void)?) {
+    public init(size: CGSize?, title: String? = nil, titleColor: UIColor? = nil, image: UIImage, highlightedImage: UIImage, backgroundImage: UIImage?, backgroundHighlightedImage: UIImage?, itemTapped: (() -> Void)?) {
         
         // Initialize properties
         //
@@ -38,28 +74,39 @@ public class ExpandingMenuItem: UIButton {
         
         super.init(frame: itemFrame)
         
-        // Configure images
+        // Configure base button
         //
-        setImage(backgroundImage, forState: UIControlState.Normal)
-        setImage(backgroundHighlightedImage, forState: UIControlState.Highlighted)
+        let baseButton = UIButton()
+        baseButton.setImage(backgroundImage, forState: UIControlState.Normal)
+        baseButton.setImage(backgroundHighlightedImage, forState: UIControlState.Highlighted)
+        baseButton.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(baseButton)
         
-        self.frontImageView.center = CGPoint(x: bounds.width / 2.0, y: bounds.height / 2.0)
-        addSubview(self.frontImageView)
+        self.addConstraint(NSLayoutConstraint(item: baseButton, attribute: .Top, relatedBy: .Equal, toItem: self, attribute: .Top, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: baseButton, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: baseButton, attribute: .Leading, relatedBy: .Equal, toItem: self, attribute: .Leading, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: baseButton, attribute: .Trailing, relatedBy: .Equal, toItem: self, attribute: .Trailing, multiplier: 1, constant: 0))
+        
         
         // Add an action for the item
         //
-        addTarget(self, action: "tapped", forControlEvents: UIControlEvents.TouchUpInside)
+        baseButton.addTarget(self, action: "tapped", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // Configure front images
+        //
+        self.frontImageView.contentMode = .Center
+        self.frontImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(self.frontImageView)
+        
+        self.addConstraint(NSLayoutConstraint(item: self.frontImageView, attribute: .Top, relatedBy: .Equal, toItem: self, attribute: .Top, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: self.frontImageView, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: self.frontImageView, attribute: .Leading, relatedBy: .Equal, toItem: self, attribute: .Leading, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: self.frontImageView, attribute: .Trailing, relatedBy: .Equal, toItem: self, attribute: .Trailing, multiplier: 1, constant: 0))
         
         // Configure title button
         //
         if let title = title {
-            let button = UIButton()
-            button.setTitle(title, forState: UIControlState.Normal)
-            button.sizeToFit()
-            
-            button.addTarget(self, action: "tapped", forControlEvents: UIControlEvents.TouchUpInside)
-            
-            self.titleButton = button
+            self.titleButton = self.createTitleButton(title, titleColor: titleColor)
         }
     }
     
@@ -67,8 +114,8 @@ public class ExpandingMenuItem: UIButton {
         self.init(size: nil, title: nil, image: image, highlightedImage: highlightedImage, backgroundImage: backgroundImage, backgroundHighlightedImage: backgroundHighlightedImage, itemTapped: itemTapped)
     }
     
-    public convenience init(title: String, image: UIImage, highlightedImage: UIImage, backgroundImage: UIImage?, backgroundHighlightedImage: UIImage?, itemTapped: (() -> Void)?) {
-        self.init(size: nil, title: title, image: image, highlightedImage: highlightedImage, backgroundImage: backgroundImage, backgroundHighlightedImage: backgroundHighlightedImage, itemTapped: itemTapped)
+    public convenience init(title: String, titleColor: UIColor? = nil, image: UIImage, highlightedImage: UIImage, backgroundImage: UIImage?, backgroundHighlightedImage: UIImage?, itemTapped: (() -> Void)?) {
+        self.init(size: nil, title: title, titleColor: titleColor, image: image, highlightedImage: highlightedImage, backgroundImage: backgroundImage, backgroundHighlightedImage: backgroundHighlightedImage, itemTapped: itemTapped)
     }
     
     public convenience init(size: CGSize, image: UIImage, highlightedImage: UIImage, backgroundImage: UIImage?, backgroundHighlightedImage: UIImage?, itemTapped: (() -> Void)?) {
@@ -79,6 +126,18 @@ public class ExpandingMenuItem: UIButton {
         self.frontImageView = UIImageView()
         
         super.init(coder: aDecoder)
+    }
+    
+    // MARK: - Title Button
+    private func createTitleButton(title: String, titleColor: UIColor? = nil) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, forState: .Normal)
+        button.setTitleColor(titleColor, forState: .Normal)
+        button.sizeToFit()
+        
+        button.addTarget(self, action: "tapped", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        return button
     }
     
     // MARK: - Tapped Action
