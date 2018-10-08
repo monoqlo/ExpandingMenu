20c34f722ca2c82bc8ed16b90d28171e8c75e84f
Kamran Khan <kamrankhan07@users.noreply.github.com>
2018-10-08 16:26:23 +0700

    Update ExpandingMenuButton.swift

--- ExpandingMenu/Classes/ExpandingMenuButton.swift
+++ ExpandingMenu/Classes/ExpandingMenuButton.swift
@@ -143,8 +143,7 @@ open class ExpandingMenuButton: UIView, UIGestureRecognizerDelegate {
             
             self.expandingSoundPath = Bundle(url: Bundle(for: ExpandingMenuButton.classForCoder()).url(forResource: "ExpandingMenu", withExtension: "bundle")!)?.path(forResource: "expanding", ofType: "caf")
             self.foldSoundPath = Bundle(url: Bundle(for: ExpandingMenuButton.classForCoder()).url(forResource: "ExpandingMenu", withExtension: "bundle")!)?.path(forResource: "fold", ofType: "caf")
-            self.selectedSoundPath = Bundle(url: Bundle(for: ExpandingMenuButton.classForCoder()).url(forResource: "ExpandingMenu", withExtension: "bundle")!)?.path(forResource: "selected", ofType: "caf")
-            
+            self.selectedSoundPath = Bundle(url: Bundle(for: ExpandingMenuButton.classForCoder()).url(forResource: "ExpandingMenu", withExtension: "bundle")!)?.path(forResource: "selected", ofType: "caf") 
         }
         
         // Configure enter and highlighted center image
